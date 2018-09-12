3a2536c7dc5e694f9de63c53a9654ed9a58145c7
Stephen Bissett <steve.bissett5@gmail.com>
2018-09-12 15:42:41 -0600

    updatewithjs

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+//* Game random number variables set betwenn 19 and 120
+//* Crystal numbers variable bewtween 1 to 12
+//* Game random number set immediately and cleared/changed at each game
+//* Player presses random crystal to increase score
+//* When player hits a crystal it will update players score
+//* Player wins if their score matches random number
+//* Player looses if their scores goes above random number
+//* Wins and Loses tallied throughout game
+//* Game restarts when player wins or looses.
+//* Game goes to 10 THEN resets
+
+// to tee up jquery with start with the $(document).ready(function() {}
+
+$(document).ready(function() { 
+
+//*create and array for computer guess options between 9 and 120
+//where the computer begins it's guesses at 19
+
+
+var compNumber = Math.floor(Math.random() *102) + 19;
+    console.log("compNumber: " + compNumber);
+
+}
+
+
+
+
+
+});
+
+
+});
