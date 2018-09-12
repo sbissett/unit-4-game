0344d2b501c1a9fa22ec68599b370c9a0d0a43a3
Stephen Bissett <steve.bissett5@gmail.com>
2018-09-12 17:30:54 -0600

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,6 @@
 //* Game random number variables set betwenn 19 and 120
 //* Crystal numbers variable bewtween 1 to 12
+//* 4 crystals so 4 additional variables
 //* Game random number set immediately and cleared/changed at each game
 //* Player presses random crystal to increase score
 //* When player hits a crystal it will update players score
@@ -14,16 +15,18 @@
 $(document).ready(function() { 
 
 //*create and array for computer guess options between 9 and 120
-//where the computer begins it's guesses at 19
+//where the computer begins it's guesses at 19 and ends at 102
 
 
 var compNumber = Math.floor(Math.random() *102) + 19;
     console.log("compNumber: " + compNumber);
-
-}
-
+    $(".randomNumber").html(compNumber);
 
 
+var diamondCrystal = Math.floor(Math.random() *12) + 1;
+    console.log("Diamond: " + diamondCrystal);
+    $("#img").html("img src=" + "assets/iamges/Diamondimage.jpb" + " value")
+    
 
 
 });
