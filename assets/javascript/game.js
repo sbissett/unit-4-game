a345521f51cfe4d28e8dc090b4311ec351411849
Stephen Bissett <steve.bissett5@gmail.com>
2018-09-13 15:37:57 -0600

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -23,13 +23,32 @@ var compNumber = Math.floor(Math.random() *102) + 19;
     $(".randomNumber").html(compNumber);
 
 
-var diamondCrystal = Math.floor(Math.random() *12) + 1;
-    console.log("Diamond: " + diamondCrystal);
-    $("#img").html("img src=" + "assets/iamges/Diamondimage.jpb" + " value")
+ 
+var gemDiamond = Math.floor(Math.random() *12 ) +1;
+    console.log("Gem Diamond: " + gemDiamond);
+    $("#imgDiamond").html("<img src=" + "assets/images/Diamondimage.jpg" + " value=" + gemDiamond + ">");
+
+var gemRuby = Math.floor(Math.random() *12) + 1;
+    console.log("Gem Ruby: " +gemRuby);
+    $("#imgRuby").html("img src=" + "assets/images/Rubyimage.jpg") + " value=" + gemRuby + ">";
+
+var gemSapphire = Math.floor(Math.random() *12 ) +1;
+    console.log("Gem Sapphire; " + gemSapphire);
+    $("#imgSapphire").html("img src" + "assets/images/Sapphireimage.jpg") + " value=" + gemSapphire + ">");
     
 
+var gemYellowcrystal = Math.floor(Math.random () *12 ) +1;
+    console.log("Gem Yellow Crystal; " + gemYellowcrystal);
+    $("#imgYellowcrystal").html("img src" + "assets/images/Yellowcrystalimage.jpg") + " value=" + gemYellowcrystal + ">";   
+
+
+//Starting with my functions
+
+
+    
+}
+
+
 
 });
 
-
-});
